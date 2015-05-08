727cbc1df2a6f22a1b2b000223b9257b14bb0edd
Anders Bälter <anders.balter@npolar.no>
2015-05-08 13:34:15 +0200

    Use callsite to get path of caller

--- index.js
+++ index.js
@@ -5,8 +5,9 @@
  * Just use rrequire('DatasetEditController') instead!
  */
 var path = require('path');
+var callsite = require('callsite');
 var rrequire = function (filename) {
-  var absPath = __dirname;
+  var absPath = path.dirname(callsite()[1].getFileName());
   var projectRoot = process.cwd();
   var projectRelativePath = absPath.replace(projectRoot, '').replace('test', 'app');
   var absSrcPath = path.join(projectRoot, projectRelativePath, filename);
